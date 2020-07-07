e93cfe4690a9ca1ddcd686e6e5487c3856abe976
Kevin Medina <kmedinakm@hotmail.com>
2020-07-06 21:49:01 -0400

    add movies and shows page to render results from API

--- client/src/components/Movies/Movies.js
+++ client/src/components/Movies/Movies.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./Movies.css";
 
-const URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=series`;
+const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}`;
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
@@ -9,7 +10,8 @@ const Movies = () => {
     axios
       .get(URL)
       .then((res) => {
-        setMovies(res.data.Search);
+        console.log(res.data.results);
+        setMovies(res.data.results);
       })
       .catch((err) => console.log(`Error: ${err}`));
   }, []);
@@ -17,9 +19,45 @@ const Movies = () => {
   return (
     <div>
       <h1>Movies</h1>
-      <div>
+
+      {/* Dropdown button for categories */}
+      <div class="dropdown">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Categories
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">
+            Action
+          </a>
+          <a class="dropdown-item" href="#">
+            Another action
+          </a>
+          <a class="dropdown-item" href="#">
+            Something else here
+          </a>
+        </div>
+      </div>
+
+      {/* Movie Container */}
+      <div className="movies-container">
         {movies.map((movie, i) => (
-          <div key={i}>{movie.Title}</div>
+          <div key={i} className="movie">
+            <img
+              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+              alt="movie poster"
+            />
+            <div>
+              <h4>{movie.original_title}</h4>
+              {/* <p>{movie.overview}</p> */}
+            </div>
+          </div>
         ))}
       </div>
     </div>
--- client/src/components/Shows/Shows.js
+++ client/src/components/Shows/Shows.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Shows.css";
+
+const URL = `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.REACT_APP_API_KEY}`;
 
 const Shows = () => {
+  const [shows, setShows] = useState([]);
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => {
+        console.log(res.data.results);
+        setShows(res.data.results);
+      })
+      .catch((err) => console.log(`Error: ${err}`));
+  }, []);
+
   return (
     <div>
       <h1>TV Shows</h1>
+      <div className="shows-container">
+        {shows.map((show, i) => (
+          <div key={i} className="show">
+            <img
+              src={`https://image.tmdb.org/t/p/w300${show.poster_path}`}
+              alt="show poster"
+            />
+            <div>
+              <h4>{show.name}</h4>
+              {/* <p>{show.overview}</p> */}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

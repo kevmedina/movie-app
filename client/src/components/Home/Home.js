5a3bad67ad77fa26ed08f9a1e25f90e00e69a1e6
Kevin Medina <kmedinakm@hotmail.com>
2020-07-09 22:56:47 -0400

    update the home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <header>
-        <h1>Welcome to Cinema 954</h1>
-        <h3>Choose from hundreds of movies or TV Shows to watch. </h3>
+        <div>
+          <h1>Welcome to Cinema 954</h1>
+          <h3>Choose from hundreds of movies or TV Shows to watch. </h3>
+        </div>
+        <div>
+          <Link to="/movies" className="link">
+            Movies
+          </Link>
+          <Link to="/shows" className="link">
+            TV Shows
+          </Link>
+        </div>
       </header>
     </div>
   );
